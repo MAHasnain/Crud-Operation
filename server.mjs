690c4a45f3c71fbc8abc4eb2db41818efa93afe7
Atta Hasnain <attaulhasnainmaqsoodi@gmail.com>
2023-09-19 09:20:13 +0500

    api complete

--- server.mjs
+++ server.mjs
@@ -1,8 +1,29 @@
 import express from "express";
+import { customAlphabet } from "nanoid";
+const nanoid = customAlphabet("1234567890", 20);
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
+let products = [
+  {
+    id: nanoid(),
+    name: "macbook",
+    price: 100,
+  },
+  {
+    id: nanoid(),
+    name: "iphone",
+    price: 200,
+  },
+  {
+    id: nanoid(),
+    name: "ipad",
+    price: 300,
+  },
+];
+
+app.get("/products", (req, res) => {
   res.send({ message: "all product found", data: products });
 });
 
@@ -16,7 +37,7 @@ app.get("/product/:id", (req, res) => {
   let isFound = false;
   for (let i = 0; i < products.length; i++) {
     if (products[i].id === req.params.id) {
-      isFound = products[i];
+      isFound = i;
       break;
     }
   }
@@ -33,16 +54,15 @@ app.get("/product/:id", (req, res) => {
 });
 
 app.post("product", (req, res) => {
-  if (!req.body.id || !req.body.name || !req.body.price) {
+  if (!req.body.name || !req.body.price) {
     res.status(403).send(`
     required parameters are missing. example JSON request body: {
-        id : 23453 /// always a number
         name : "macbook" /// always a string
         price : 100
     }`);
   }
   products.push({
-    id: req.body.id,
+    id: nanoid(), /// generate
     name: req.body.name,
     price: req.body.price,
   });
@@ -50,23 +70,55 @@ app.post("product", (req, res) => {
   res.status(201).send({ message: "product created" });
 });
 
-let products = [
-  {
-    id: 152349785457,
-    name: "macbook",
-    price: 100,
-  },
-  {
-    id: 152349785458,
-    name: "iphone",
-    price: 200,
-  },
-  {
-    id: 152349785459,
-    name: "ipad",
-    price: 300,
-  },
-];
+app.put("/product/:id", (req, res) => {
+  if (!req.body.name && !req.body.price) {
+    `
+    res.status(403).send{
+      required parameter are missing. Atleast one parameter is required : name or price to complete update.
+      example JSON request body: {name : "macbook", price : 100}`;
+  }
+
+  let isFound = false;
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === req.params.id) {
+      isFound = i;
+      break;
+    }
+  }
+
+  if (isFound === false) {
+    res.status(404);
+    res.send({ message: "Product not found" });
+  } else {
+    if (req.body.name) products[isFound].name = req.body.name;
+    if (req.body.price) products[isFound].price = req.body.price;
+
+    res.send({
+      message: "product is updated with id :" + products[isFound].id,
+      data: products[isFound],
+    });
+  }
+});
+
+app.delete("/product", (req, res) => {
+  let isFound = false;
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === req.params.id) {
+      isFound = i;
+      break;
+    }
+  }
+
+  if (isFound === false) {
+    res.status(404);
+    res.send({ message: "Product not found" });
+  } else {
+    products.splice(isFound, 1);
+    res.send({
+      message: "product is deleted",
+    });
+  }
+});
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
